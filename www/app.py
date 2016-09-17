3c25e41fa6720dba3089df87ed2b99702c7b38af
ZsnnsZ <925453683@qq.com>
2016-09-17 20:56:05 +0800

    day 12,add some animations

--- www/app.py
+++ www/app.py
@@ -126,13 +126,13 @@ def response_factory(app, handler):
 def datetime_filter(t):
     delta = int(time.time() - t)
     if delta < 60:
-        return u'1分钟前'
+        return u'1分钟 前'
     if delta < 3600:
-        return u'%s分钟前' % (delta // 60)# //表示整除
+        return u'%s分钟 前' % (delta // 60)# //表示整除
     if delta < 86400:
-        return u'%s小时前' % (delta // 3600)
+        return u'%s小时 前' % (delta // 3600)
     if delta < 604800:
-        return u'%s天前' % (delta // 86400)
+        return u'%s天 前' % (delta // 86400)
     dt = datetime.fromtimestamp(t)
     return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
